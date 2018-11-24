b568424a7b27ccb72b179dad29e036edeaebcd62
Sagnik Basu <sbasu4@visteon.com>
2018-11-24 19:36:25 +0530

    compilation error fix

--- common/algorithms/circular_buff.c
+++ common/algorithms/circular_buff.c
@@ -2,45 +2,49 @@
 #include "circular_buff.h"
 
 
-void edge_os_circular_buff_init(edge_os_circular_buff *buff, uint32_t size)
+int  edge_os_circular_buff_init(struct edge_os_circular_buff *buff, uint32_t size)
 {
 
-    buff = calloc(1,size*sizeof(edge_os_circular_buff));
+    buff = calloc(1,size*sizeof(struct edge_os_circular_buff));
+    if (!buff) {
+        return -1;
+    }
     buff->max_size = size;
     edge_os_circular_buff_reset(buff);
+    return 1;
 }
 
 
-void egde_os_circular_buff_reset(edge_os_circular_buff *buff)
+void egde_os_circular_buff_reset(struct edge_os_circular_buff *buff)
 {
     buff->read_len = 0;
     buff->write_len = 0;
     buff->max_size = 0;
 }
 
-void edge_os_circular_buff_free(edge_os_circular_buff *buff)
+void edge_os_circular_buff_free(struct edge_os_circular_buff *buff)
 {
     assert(buff);
     free(buff);
 }
 
-void edge_os_circular_buff_write(edge_os_circular_buff *buff, void* data)
+void edge_os_circular_buff_write(struct edge_os_circular_buff *buff, void* data)
 {
-    buff->data[write_len] = data;
+    buff->data[buff->write_len] = data;
     buff->write_len = (buff->write_len + 1);
     edge_os_circular_buff_adjust(buff);
 }
 
-void edge_os_circular_buff_read(edge_os_circular_buff *buff, void* data)
+void edge_os_circular_buff_read(struct edge_os_circular_buff *buff, void* data)
 {
-    data = buff->data[read_len];
+    data = buff->data[buff->read_len];
     edge_os_circular_buff_adjust(buff);
     buff->read_len = (buff->read_len + 1);
 }
 
-void edge_os_circular_buff_adjust(edge_os_circular_buff *buff)
+void edge_os_circular_buff_adjust(struct edge_os_circular_buff *buff)
 {
-    if (buff->write_len == max_size) {
+    if (buff->write_len == buff->max_size) {
         buff->write_len = 0;
     }
     else if(buff->read_len >= buff->write_len) {
--- common/algorithms/circular_buff.h
+++ common/algorithms/circular_buff.h
@@ -8,10 +8,10 @@ struct edge_os_circular_buff {
     void *data;
     uint32_t read_len;
     uint32_t write_len;
-    uint32_t size;
+    uint32_t max_size;
 };
 
-void edge_os_circular_buff_init(struct edge_os_circular_buff *buff, unint32_t SIZE);
+int  edge_os_circular_buff_init(struct edge_os_circular_buff *buff, uint32_t SIZE);
 void edge_os_list_read(struct edge_os_list_base *buff, void *data);
 void edge_os_circular_buff_write(struct edge_os_circular_buff *buff, void *data);
 void edge_os_circular_buff_free(edge_os_circular_buff *buff);
