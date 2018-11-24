5b9e39d1d08ac0a4ae6533c5a91d771d257d1378
Sagnik Basu <sbasu4@visteon.com>
2018-11-24 19:29:21 +0530

    implement circular buffer

--- common/algorithms/circular_buff.c
+++ common/algorithms/circular_buff.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
-#include <circular_buff.h>
+#include "circular_buff.h"
 
 
 void edge_os_circular_buff_init(edge_os_circular_buff *buff, uint32_t size)
 {
 
-    buff = malloc(sizeof(edge_os_circular_buff));
+    buff = calloc(1,size*sizeof(edge_os_circular_buff));
+    buff->max_size = size;
     edge_os_circular_buff_reset(buff);
 }
 
@@ -14,7 +15,7 @@ void egde_os_circular_buff_reset(edge_os_circular_buff *buff)
 {
     buff->read_len = 0;
     buff->write_len = 0;
-    buff->full = false;
+    buff->max_size = 0;
 }
 
 void edge_os_circular_buff_free(edge_os_circular_buff *buff)
@@ -22,3 +23,29 @@ void edge_os_circular_buff_free(edge_os_circular_buff *buff)
     assert(buff);
     free(buff);
 }
+
+void edge_os_circular_buff_write(edge_os_circular_buff *buff, void* data)
+{
+    buff->data[write_len] = data;
+    buff->write_len = (buff->write_len + 1);
+    edge_os_circular_buff_adjust(buff);
+}
+
+void edge_os_circular_buff_read(edge_os_circular_buff *buff, void* data)
+{
+    data = buff->data[read_len];
+    edge_os_circular_buff_adjust(buff);
+    buff->read_len = (buff->read_len + 1);
+}
+
+void edge_os_circular_buff_adjust(edge_os_circular_buff *buff)
+{
+    if (buff->write_len == max_size) {
+        buff->write_len = 0;
+    }
+    else if(buff->read_len >= buff->write_len) {
+        buff->read_len = buff->read_len % buff->max_size ; 
+    }
+
+}
+
--- common/algorithms/circular_buff.h
+++ common/algorithms/circular_buff.h
@@ -1,14 +1,14 @@
 #ifndef __EDGE_CIRCULAR_BUFF_H__
 #define __EDGE_CIRCULAR_BUFF_H__
 
-#include <stdlib.h>
+#include <stdint.h>
 
 
 struct edge_os_circular_buff {
     void *data;
     uint32_t read_len;
     uint32_t write_len;
-    unin32_t size;
+    uint32_t size;
 };
 
 void edge_os_circular_buff_init(struct edge_os_circular_buff *buff, unint32_t SIZE);
