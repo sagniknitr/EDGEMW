408cf234893678f3528b00adcb1a5a1cf1c4c231
Dev <ndevendr@visteon.com>
2018-12-23 21:44:20 -0500

    have a global initialiser for the lib

--- common/logger/edgeos_logger.c
+++ common/logger/edgeos_logger.c
@@ -13,6 +13,7 @@ struct edge_os_logger_ctx {
     int logfd_;
     char *ipaddr;
     int port;
+    int fallback_to_local;
 };
 
 void *edge_os_logger_init(char *ipaddr, int port)
@@ -33,7 +34,16 @@ void *edge_os_logger_init(char *ipaddr, int port)
     handle->ipaddr = strdup(ipaddr);
     handle->port = port;
 
+    handle->fallback_to_local = 0;
+
     return handle;
+}
+
+void edge_os_set_logger_fallback_local(void *handle)
+{
+    struct edge_os_logger_ctx *_l = handle;
+
+    _l->fallback_to_local = 1;
 }
 
 void edge_os_logger_deinit(void *handle)
@@ -52,6 +62,7 @@ int edge_os_logger_writemsg(void *handle, char *fmt, ...)
     struct edge_os_logger_ctx *l;
     char buf[4096];
     va_list arg;
+    int ret;
     int len;
 
     l = handle;
@@ -60,6 +71,35 @@ int edge_os_logger_writemsg(void *handle, char *fmt, ...)
     len = vsnprintf(buf, sizeof(buf), fmt, arg);
     va_end(arg);
 
-    return edge_os_udp_sendto(l->logfd_, buf, len, l->ipaddr, l->port);
+    ret = edge_os_udp_sendto(l->logfd_, buf, len, l->ipaddr, l->port);
+    if (ret < 0) {
+        if (l->fallback_to_local) {
+            edge_os_err(fmt, buf);
+        }
+    }
+
+    return 0;
 }
+
+int edge_os_logger_write_valist(void *handle, char *fmt, va_list arg)
+{
+    struct edge_os_logger_ctx *_l = handle;
+    char buf[4096];
+    int len;
+    int ret;
+
+    len = vsnprintf(buf, sizeof(buf), fmt, arg);
+
+    ret = edge_os_udp_sendto(_l->logfd_, buf, len, _l->ipaddr, _l->port);
+    if (ret < 0) {
+        if (_l->fallback_to_local) {
+            edge_os_err(fmt, buf);
+        }
+    }
+
+    return 0;
+}
+
+
+
 
--- common/logger/edgeos_logger.h
+++ common/logger/edgeos_logger.h
@@ -2,6 +2,7 @@
 #define __EOS_LOGGER_H__
 
 #include <syslog.h>
+#include <stdarg.h>
 
 #define edge_os_log(fmt, ...) { \
     syslog(LOG_USER | LOG_INFO, fmt, ##__VA_ARGS__); \
@@ -22,6 +23,8 @@
 void *edge_os_logger_init(char *ipaddr, int port);
 void edge_os_logger_deinit(void *handle);
 int edge_os_logger_writemsg(void *handle, char *fmt, ...);
+int edge_os_logger_write_valist(void *handle, char *fmt, va_list arg);
+void edge_os_set_logger_fallback_local(void *handle);
 
 #endif
 
