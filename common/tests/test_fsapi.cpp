20d525fd24cb9e0573cec40ecc3d58b68f95f479
Dev <devendra.aaru@gmail.com>
2018-12-24 02:44:54 -0500

    fsapi test

---
+++ common/tests/test_fsapi.cpp
@@ -0,0 +1,78 @@
+#include <iostream>
+#include <cstring>
+extern "C" {
+#include <fsapi.h>
+}
+
+class fsAPITests {
+    private:
+        int fd_;
+
+        void testOpenNewFail()
+        {
+            edgeos_create_file(NULL);
+        }
+
+        void testOpenNewSuccess(const char *file)
+        {
+            edgeos_create_file(file);
+        }
+
+        void testFileRead(const char *file)
+        {
+            char data[1024];
+            int fd;
+            int ret;
+
+            fd = edgeos_open_file(file, "r");
+            if (fd < 0)
+                return;
+
+            while (1) {
+                ret = edgeos_read_file(fd, data, sizeof(data) - 1);
+                if (ret <= 0)
+                    break;
+
+                edgeos_write_file(2, data, ret);
+            }
+
+            edgeos_close_file(fd);
+        }
+
+        void testFileWrite(const char *file)
+        {
+            char data[] = "write something to file\n";
+            int fd;
+            int i;
+
+            fd = edgeos_create_file(file);
+            if (fd < 0)
+                return;
+
+            for (i = 0; i < 10000; i ++) {
+                edgeos_write_file(fd, data, strlen(data));
+            }
+
+            edgeos_close_file(fd);
+        }
+
+
+    public:
+        fsAPITests() { }
+        ~fsAPITests() { }
+        void testAll()
+        {
+            testOpenNewFail();
+            testOpenNewSuccess("./t");
+            testFileWrite("./t");
+            testFileRead("./t");
+        }
+};
+
+int main()
+{
+    fsAPITests t;
+
+    t.testAll();
+}
+
