cd53e20c19fef945d925ed7e290e170c54246ab7
Dev <devendra.aaru@gmail.com>
2018-12-25 14:13:32 -0500

    rename csvparse

---
+++ common/util/csvclass.hpp
@@ -0,0 +1,14 @@
+#ifndef __CSV_CLASS_HPP__
+#define __CSV_CLASS_HPP__
+
+#include <vector>
+#include <string>
+
+
+class csvClass {
+    public:
+        int csvParse(std::string input, std::vector<std::string> &output);
+};
+
+#endif
+
