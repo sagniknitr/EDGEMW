553a25122d0e5641065ad4bd420e73f53e02bcee
Dev <devendra.aaru@gmail.com>
2018-12-25 14:57:14 -0500

    csv reader and writer for the file

--- common/util/csvclass.hpp
+++ common/util/csvclass.hpp
@@ -3,11 +3,28 @@
 
 #include <vector>
 #include <string>
-
+extern "C" {
+#include <stdio.h>
+#include <stdint.h>
+}
 
 class csvClass {
     public:
+        csvClass();
+        ~csvClass();
+        csvClass(std::string filename, std::string mode);
+        int csvReadLine(std::string & line, std::vector<std::string> &output);
+        int csvWriteLine(std::vector<std::string> rowInfo);
+        int csvWriteLineBuf();
+        void csvAppendLine(std::string line, int val, int eol);
+        void csvAppendLine(int val, int eol);
+        void csvAppendLine(std::string line, double val, int eol);
+        void csvAppendLine(double val, int eol);
         int csvParse(std::string input, std::vector<std::string> &output);
+
+    private:
+        FILE *fp;
+        std::string line_;
 };
 
 #endif
