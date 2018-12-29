2bc4f968b929e8a74a4f6045d2505307947b9b31
Dev <devendra.aaru@gmail.com>
2018-12-29 11:27:38 -0500

    build

--- build.sh
+++ build.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
 
 run_gcc_tests() {
-    cd external/protobuf/
-    ./autogen.sh
-    ./configure
-    make -j12
-    sudo make install
-    sudo ldconfig
+#    cd external/protobuf/
+#    ./autogen.sh
+#    ./configure
+#    make -j12
+#    sudo make install
+#    sudo ldconfig
 
-    cd ../../
+#    cd ../../
 
-    cd external/protobuf-c/
-    ./autogen.sh
-    ./configure
-    make -j12
-    sudo make install
-    sudo ldconfig
+#    cd external/protobuf-c/
+#    ./autogen.sh
+#    ./configure
+#    make -j12
+#    sudo make install
+#    sudo ldconfig
 
-    cd ../../
+#    cd ../../
 
     rm -rf build/
     mkdir -p build/
