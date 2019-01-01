083a93550d19634a99ed81a456889cfa00df0923
Dev <devendra.aaru@gmail.com>
2018-12-31 18:24:43 -0600

    key buf and file update

--- lib/crypto/keygen.c
+++ lib/crypto/keygen.c
@@ -1,7 +1,49 @@
 #include <stdio.h>
+#include <stdint.h>
 #include <wolfssl/wolfcrypt/sha256.h>
 #include <wolfssl/wolfcrypt/random.h>
 #include <wolfssl/wolfcrypt/pwdbased.h>
+
+int edge_os_crypto_key_create(uint8_t *key, int keysize)
+{
+    int ret;
+    int size = keysize;
+
+    RNG rng;
+    byte *tmp_ = NULL;
+
+    tmp_ = calloc(1, size);
+    if (!tmp_) {
+        goto bad;
+    }
+
+    ret = wc_InitRng(&rng);
+    if (ret < 0) {
+        goto bad;
+    }
+
+    ret = wc_RNG_GenerateBlock(&rng, key, size);
+    if (ret < 0)
+        goto bad;
+
+    ret = wc_RNG_GenerateBlock(&rng, tmp_, size);
+    if (ret < 0)
+        goto bad;
+
+    ret = wc_PBKDF2(key, tmp_, size, NULL, 0, 4096, size, SHA256);
+    if (ret < 0)
+        goto bad;
+
+    free(tmp_);
+
+    return 0;
+
+bad:
+    if (tmp_)
+        free(tmp_);
+
+    return -1;
+}
 
 int edge_os_crypto_key_gen(char *filename, int keysize)
 {
@@ -19,44 +61,13 @@ int edge_os_crypto_key_gen(char *filename, int keysize)
         return -1;
     }
 
-    RNG rng;
-    byte *key = NULL;
-    byte *tmp_ = NULL;
-    byte *salt = NULL;
+    uint8_t *key;
 
     key = calloc(1, size);
-    if (!key) {
-        goto bad;
-    }
-
-    tmp_ = calloc(1, size);
-    if (!tmp_) {
-        goto bad;
-    }
-
-    salt = calloc(1, size);
-    if (!salt) {
-        goto bad;
-    }
-
-    ret = wc_InitRng(&rng);
-    if (ret < 0) {
-        goto bad;
-    }
-
-    ret = wc_RNG_GenerateBlock(&rng, key, size);
-    if (ret < 0)
+    if (!key)
         goto bad;
 
-    ret = wc_RNG_GenerateBlock(&rng, tmp_, size);
-    if (ret < 0)
-        goto bad;
-
-    ret = wc_RNG_GenerateBlock(&rng, salt, size);
-    if (ret < 0)
-        goto bad;
-
-    ret = wc_PBKDF2(key, tmp_, size, salt, size, 4096, size, SHA256);
+    ret = edge_os_crypto_key_create(key, size);
     if (ret < 0)
         goto bad;
 
@@ -70,20 +81,12 @@ int edge_os_crypto_key_gen(char *filename, int keysize)
     fclose(fp);
 
     free(key);
-    free(tmp_);
-    free(salt);
 
     return 0;
 
 bad:
     if (key)
         free(key);
-
-    if (tmp_)
-        free(tmp_);
-
-    if (salt)
-        free(salt);
 
     return -1;
 }
