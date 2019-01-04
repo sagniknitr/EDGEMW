f7c442a752119c46173be2220424b95b743e149f
Dev <devendra.aaru@gmail.com>
2019-01-04 12:46:14 -0500

    fsapi more about access, directory and scan permissions added + semaphore build moved to two build variants (bleeding edge and stable)

--- lib/linux/file_system/fsapi.h
+++ lib/linux/file_system/fsapi.h
@@ -1,12 +1,77 @@
 #ifndef __EOS_FSAPI_H__
 #define __EOS_FSAPI_H__
 
+/**
+ * file access mode permissions
+ */
+typedef enum {
+    // read mode
+    EDGE_OS_ACCESS_READ_OK      = 0x01,
+
+    // write mode
+    EDGE_OS_ACCESS_WRITE_OK     = 0x02,
+
+    // exe mode
+    EDGE_OS_ACCESS_EXE_OK       = 0x04,
+} edge_os_access_mode_t;
+
+/**
+ * @brief - create file
+ *
+ * @param filename - must be valid pointer
+ *
+ * Description-
+ *
+ * create a file with given filename. If null pointer passed, the API fails
+ * @returns - returns file descriptor fd and -1 on error
+ */
 int edgeos_create_file(const char *filename);
 
+/**
+ * @brief - create file with truncated mode (useful for creating fixed size file)
+ *
+ * @param filename - must be valid pointer
+ * @param filesize - file size in bytes
+ *
+ * Description-
+ *
+ * create a file with given filename and truncate it. If null is passed or the filesize is
+ * negative value, the API fails.
+ *
+ * @returns - returns file descriptor fd and -1 on error
+ */
 int edgeos_create_file_truncated(const char *filename, const int filesize);
 
+/**
+ * @brief - write to the file pointed by the fd
+ *
+ * @param fd - file descriptor (returned from os_create_file or os_open_file
+ * @param msg - message pointer
+ * @param msg_len - length of the message
+ *
+ * Description-
+ *
+ * write to the file pointed by fd. API never checks for msg and msg_len validity
+ * for performance reasons
+ *
+ * @returns - returns number of bytes written on success -1 on failure
+ */
 int edgeos_write_file(int fd, void *msg, int msg_len);
 
+/**
+ * @brief - read file pointed by the fd
+ *
+ * @param fd - file descriptor (returned from os_open_file
+ * @param msg - mesasge pointer
+ * @param msg_len - length of the message
+ *
+ * Description-
+ *
+ * read file pointed by fd. API never checks for msg and msg_len validity
+ * for performance reasons
+ *
+ * @returns - returns number of bytes written on success -1 on failure
+ */
 int edgeos_read_file(int fd, void *msg, int msg_len);
 
 int edgeos_close_file(int fd);
@@ -31,6 +96,16 @@ int edgeos_file_in_directory(const char *dir, const char *filename);
 int edgeos_create_directory(const char *dir, int owner, int group, int other);
 
 int edge_os_write_file2(const char *file, void *msg, int msg_len);
+
+int edge_os_file_accessible(const char *file, edge_os_access_mode_t mode);
+
+int edge_os_create_directory_recurse(const char *dir, int owner, int group, int other);
+
+int edge_os_remove_directory(const char *dir);
+
+int edgeos_scan_directory_recurse(const char *dir,
+                                  void *priv,
+                                  void (*scan_callback)(char *fullpath, int is_dir, void *priv));
 
 #endif
 
